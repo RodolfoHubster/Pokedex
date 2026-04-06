d0b40e20456d13d66e3d92961a952b763a2ed094
RodolfoHubster <rodolfo.huitron@uabc.edu.mx>
2026-04-06 08:30:07 -0700

    fix: Remove duplicate detail display and improve navigation in Pokémon view

--- resources/views/pokemon/show.blade.php
+++ resources/views/pokemon/show.blade.php
@@ -1,3 +1,3 @@
 <h1>Estas viendo los detalles de: <strong>{{ ucfirst($name) }}</strong></p>
-
-<p>Estas viendo los detalles de: <strong>{{ ucfirst($name) }}</strong></p>
+<br>
+<a href="/pokemon">Volver al listado</a>
